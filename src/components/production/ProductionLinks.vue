95a97b42b02c9eaf35e1f5831832bf213a9cb98a
Abhishekfpaipl <131144777+Abhishekfpaipl@users.noreply.github.com>
2023-07-04 11:05:25 +0530

    ProductionBottomnav

--- src/components/production/ProductionLinks.vue
+++ src/components/production/ProductionLinks.vue
@@ -1,35 +1,37 @@
 <template>
 
-   <TopNav> Production</TopNav>
+    <TopNav> Production</TopNav>
     <div class="nav nav-tabs py-2 d-flex overflow-x-scroll flex-nowrap ws-nowarp align-items-center"
-        style="font-size: 10px; height: 63px; font-weight: 500;" id="nav-tab" role="tablist">
+    style="font-size: 10px; height: 63px; font-weight: 500;" id="nav-tab" role="tablist">
+    
+    <RouterLink class="nav-link ws-nowarp" :to="{ name: 'Production-Order' }"
+    :class="currentRouteName('Production-Order') ? 'active' : ''">Order
+</RouterLink>
 
-        <RouterLink class="nav-link ws-nowarp" :to="{ name: 'Production-Order' }"
-            :class="currentRouteName('Production-Order') ? 'active' : ''">Order
-        </RouterLink>
 
 
+<router-link class="nav-link ws-nowarp" :to="{ name: 'Production-Stiching-Single' }"
+:class="currentRouteName('Production-Stiching') ? 'active' : ''">Stiching</router-link>
 
-        <router-link class="nav-link ws-nowarp" :to="{ name: 'Production-Stiching-Single' }"
-            :class="currentRouteName('Production-Stiching') ? 'active' : ''">Stiching</router-link>
+<router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Job-Threading' }"
+:class="currentRouteName('Production-Job') ? 'active' : ''">Job
+Work</router-link>
 
-        <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Job-Threading' }"
-            :class="currentRouteName('Production-Job') ? 'active' : ''">Job
-            Work</router-link>
-
-        <!-- <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Material-One' }"
-            :class="currentRouteName('Production-Material') ? 'active' : ''">Material</router-link>
-
-        <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Overheads' }"
-            :class="currentRouteName('Production-Overheads') ? 'active' : ''">Overheads</router-link> -->
-
-        <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Finishing' }"
-            :class="currentRouteName('Production-Finishing') ? 'active' : ''">Completed</router-link>
-
-    </div>
+<!-- <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Material-One' }"
+    :class="currentRouteName('Production-Material') ? 'active' : ''">Material</router-link>
+    
+    <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Overheads' }"
+    :class="currentRouteName('Production-Overheads') ? 'active' : ''">Overheads</router-link> -->
+    
+    <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Finishing' }"
+    :class="currentRouteName('Production-Finishing') ? 'active' : ''">Completed</router-link>
+    
+</div>
+<ProductionBottomnav></ProductionBottomnav>
 </template>
 
 <script>
+import ProductionBottomnav from '../navbar/ProductionBottomnav.vue';
 import TopNav from '../navbar/TopNav.vue';
 export default {
     name: "ProductionLinks",
@@ -51,7 +53,7 @@ export default {
             return this.$route.name.includes(routeName);
         }
     },
-    components: { TopNav }
+    components: { TopNav, ProductionBottomnav }
 }
 </script>
 <style>
