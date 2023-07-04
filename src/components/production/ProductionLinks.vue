665af9d9659a54d68f07b6fc869d439ebef0f6a9
nikhil fpaipl <frontendjr1@fpaipl.com>
2023-07-04 11:06:10 +0530

    Processing Page

--- src/components/production/ProductionLinks.vue
+++ src/components/production/ProductionLinks.vue
@@ -7,23 +7,23 @@
         <RouterLink class="nav-link ws-nowarp" :to="{ name: 'Production-Order' }"
             :class="currentRouteName('Production-Order') ? 'active' : ''">Order
         </RouterLink>
-
-
-
         <router-link class="nav-link ws-nowarp" :to="{ name: 'Production-Stiching-Single' }"
-            :class="currentRouteName('Production-Stiching') ? 'active' : ''">Stiching</router-link>
+            :class="currentRouteName('Production-Stiching') ? 'active' : ''">Job</router-link>
 
         <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Job-Threading' }"
-            :class="currentRouteName('Production-Job') ? 'active' : ''">Job
-            Work</router-link>
+            :class="currentRouteName('Production-Job') ? 'active' : ''">Pending </router-link>
 
+        <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Material-One' }"
+            :class="currentRouteName('Production-Material') ? 'active' : ''">Due</router-link>
         <!-- <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Material-One' }"
             :class="currentRouteName('Production-Material') ? 'active' : ''">Material</router-link>
 
         <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Overheads' }"
+            :class="currentRouteName('Production-Overheads') ? 'active' : ''">Loss</router-link>
             :class="currentRouteName('Production-Overheads') ? 'active' : ''">Overheads</router-link> -->
 
         <router-link class="nav-link ws-nowarp " :to="{ name: 'Production-Finishing' }"
+            :class="currentRouteName('Production-Finishing') ? 'active' : ''">Finish</router-link>
             :class="currentRouteName('Production-Finishing') ? 'active' : ''">Completed</router-link>
 
     </div>
